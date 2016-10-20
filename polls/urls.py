33d10ec022cbd82e9e7ae701eee8a27789c9c304
VINCENT <nguyvincent@yahoo.com>
2016-10-20 02:57:39 -0500

    First commit

---
+++ polls/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import url
+
+from . import views
+
+#namespacing to differentiate the URLs between apps
+app_name = 'polls'
+
+urlpatterns = [
+
+	# ex: /polls/
+	url(r'^$', views.IndexView.as_view(), name='index'),
+	#ex: /polls/5/ # the 'name' value as called by the {% url %} template tag
+	url(r'^(?P<pk>[0-9]+)/$',views.DetailView.as_view(), name='detail'),
+	#ex: /polls/5/results/
+	url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
+	#ex: polls/5/vote/
+	url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+]
+
